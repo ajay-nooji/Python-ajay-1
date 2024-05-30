adade55f695019fce8ac4dc5adae89add6f71569
Ajay <85278757+ajay-nooji@users.noreply.github.com>
2024-05-30 15:25:31 +0530

    Math

---
+++ Math Divmod.py
@@ -0,0 +1,2 @@
+print("{0}\n{1}\n({0}, {1})".format(*divmod(int(input()), int(input()))))
+input()
---
+++ power mod power.py
@@ -0,0 +1,3 @@
+a, b, m = int(input()), int(input()), int(input())
+print("{0}\n{1}".format(pow(a, b), pow(a, b, m)))
+input()
